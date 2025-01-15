8a0a258072e7a15f7af4ce5f5bd0ab0b48174c68
SergWebAcceptance <serg.web@acceptance.ltd>
2025-01-15 14:15:08 +0200

    added standarts page

--- src/app/aIris-standards/_components/StandartsHero.jsx
+++ src/app/aIris-standards/_components/StandartsHero.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { fadeInUp } from "@/utils/animations";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 const StandartsHero = () => {
   return (
@@ -9,22 +10,50 @@ const StandartsHero = () => {
       <img src="/images/about/ellipse1.png" />
       <div className="_container">
         <div className="standarts-hero__body">
-          <motion.h1
+          <motion.div
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
             variants={fadeInUp}
           >
-            AIris Standards
-          </motion.h1>
-          <motion.h2
+            <Image
+              alt="hero1"
+              src={"/images/standarts/hero1.png"}
+              width={330}
+              height={300}
+            />
+          </motion.div>
+          <div>
+            <motion.h1
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={fadeInUp}
+            >
+              AIris Standards
+            </motion.h1>
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={fadeInUp}
+            >
+              Our commitment to ethical and secure practices.
+            </motion.h2>
+          </div>
+          <motion.div
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
             variants={fadeInUp}
           >
-            Our commitment to ethical and secure practices.
-          </motion.h2>
+            <Image
+              alt="hero1"
+              src={"/images/standarts/hero2.png"}
+              width={330}
+              height={300}
+            />
+          </motion.div>
         </div>
       </div>
     </section>
--- src/app/aIris-standards/page.jsx
+++ src/app/aIris-standards/page.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "@/styles/standarts.scss";
 import ContactUs from "@/components/ContactUs";
 import StandartsHero from "./_components/StandartsHero";
+import StandartsLoop from "./_components/StandartsLoop";
 
 const Standarts = () => {
   return (
     <>
       <StandartsHero />
+      <StandartsLoop />
       <ContactUs
         image="/images/standarts/contact.png"
         title="<span>Have Questions?</span> Contact Us!"
